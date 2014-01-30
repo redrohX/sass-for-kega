79583082a37c43f7350869f9bd90a59aa2ecd620
Niels van Rongen <vanrongen@gmail.com>
2014-01-30 21:51:47 +0100

    + Placeholder selectors. Grunt minify CSS. Styled buttons.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,7 @@
 module.exports = function(grunt) {
 
-    // 1. All configuration goes here 
     grunt.initConfig({
         autoprefixer: {
-            // prefix the specified file
             single_file: {
                 src: 'css/main.css',
                 dest: 'css/main.css'
@@ -17,20 +15,26 @@ module.exports = function(grunt) {
                 }
             }
         },
+
+        cssmin: {
+            minify: {
+                src: 'css/main.css',
+                dest: 'css/main.css'
+            }
+        },
+
         watch: {
             css: {
                 files: '**/*.scss',
-                tasks: ['sass', 'autoprefixer']
+                tasks: ['sass', 'autoprefixer', 'cssmin']
             }
         }
     });
 
-    // 3. Where we tell Grunt we plan to use this plug-in.
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
 
-    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
     grunt.registerTask('default', ['watch']);
-
 }
